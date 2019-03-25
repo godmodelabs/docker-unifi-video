426ae9d52e816f4a7375da72a99a4bcac403d93f
rstumbaum <rainer.stumbaum@boerse-go.de>
2019-03-25 15:59:15 +0100

    make sure Java process runs as PID 1

--- Dockerfile
+++ Dockerfile
@@ -1,43 +1,15 @@
-FROM lsiobase/xenial
+FROM openjdk:8-jre-stretch
 
-ARG BUILD_DATE
-ARG VCS_REF
+ADD https://dl.ubnt.com/firmwares/ufv/v3.9.12/unifi-video.Debian7_amd64.v3.9.12.deb unifi-video.deb
 
-# package versions
-ARG UNIFI_VIDEO_VER="3.9.12"
-
-# environment settings
-ARG DEBIAN_FRONTEND="noninteractive"
-
-LABEL org.label-schema.build-date=$BUILD_DATE \
-      org.label-schema.vcs-ref=$VCS_REF \
-      org.label-schema.vcs-url="https://github.com/kc8apf/docker-unifi-video" \
-      org.label-schema.version=$UNIFI_VIDEO_VER \
-      org.label-schema.schema-version="1.0"
-
-# add mongo unifi-video repos
-RUN \
- apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 82C6571E && \
- echo "deb [arch=amd64] http://www.ubnt.com/downloads/unifi-video/apt-3.x xenial ubiquiti" >> /etc/apt/sources.list.d/unifi-video.list
-
-# install packages
-RUN \
- apt-get update && \
- apt-get install -y \
-    unifi-video=${UNIFI_VIDEO_VER}
-
-# cleanup
-RUN \
- apt-get clean && \
- rm -rf \
-	/tmp/* \
-	/var/lib/apt/lists/* \
-	/var/tmp/*
-
-# add local files
-COPY root/ /
-
-# Volumes and Ports
-WORKDIR /usr/lib/unifi-video
+RUN apt update -q -y &&\
+    apt install -q -y sudo psmisc lsb-release mongodb jsvc &&\
+    dpkg -i unifi-video.deb
+    
 VOLUME /config
 EXPOSE 7080/tcp 7443/tcp 6666/tcp 7442/tcp 7445/tcp 7446/tcp 7447
+WORKDIR /usr/lib/unifi-video
+
+USER unifi-video
+
+ENTRYPOINT ENTRYPOINT.sh
--- ENTRYPOINT.sh
+++ ENTRYPOINT.sh
@@ -1,23 +1,24 @@
-#!/usr/bin/with-contenv bash
-
-NAME=unifi-video
+#!/bin/sh
 
 BASEDIR="/usr/lib/${NAME}"
 DATADIR="/config/data"
 
-# create our folders
+# create our folders
 mkdir -p \
-	/config/{data,logs}
+        /config/{data,logs}
 
-# create symlinks for config
+# Relink the DATADIR 
 [[ -L ${BASEDIR}/data && ! ${BASEDIR}/data -ef /config/data ]] && unlink ${BASEDIR}/data
 [[ -L ${BASEDIR}/logs && ! ${BASEDIR}/logs -ef /config/logs ]] && unlink ${BASEDIR}/logs
 [[ ! -L ${BASEDIR}/data ]] && ln -s /config/data ${BASEDIR}/data
 [[ ! -L ${BASEDIR}/logs ]] && ln -s /config/logs ${BASEDIR}/logs
 
-# start the wizard if no existing config is present
+# Start the wizard if no existing config is present
 [[ ! -f /config/data/system.properties ]] && cp -f ${BASEDIR}/etc/system.properties /config/data/
 [[ ! -f /config/data/ufv-truststore ]] && cp -f ${BASEDIR}/etc/ufv-truststore /config/data/
 
-# permissions
-find /config ! -user unifi-video ! -group unifi-video -exec chown -R unifi-video:unifi-video {} \;
+# Set permissions if necessary
+find /config ! -user unifi-video -exec chown -R unifi-video {} \;
+
+# Start application
+java -cp /usr/share/java/commons-daemon.jar:/usr/lib/unifi-video/lib/airvision.jar -Djava.security.egd=file:/dev/./urandom -Djava.library.path=/usr/lib/unifi-video/lib -Djava.awt.headless=true -Djavax.net.ssl.trustStore=/usr/lib/unifi-video/data/ufv-truststore -Dfile.encoding=UTF-8 com.ubnt.airvision.Main start
